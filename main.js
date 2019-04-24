eb9a83eb00f5d7447ad64f129fa6378d848e72c8
rakeshpuhan07 <rakeshpuhan07@gmail.comgit configit config --global user.email rakeshpuhan07@gmail.comgit commit -m first>
2019-04-25 01:51:29 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+( function ()
+{
+    var app = angular.module('myApp',[]);
+    app.controller('MyController',['$scope',myController]);
+
+    var excelJsonObj = [];
+    function myController($scope){
+    	$scope.uploadExcel = function()
+    	{
+    		var myFile = document.getElementById('file');
+    		var input = myFile;
+    		var reader = new FileReader();
+    		reader.onload = function(){
+    			var fileData = reader.result;
+    			var workbook = XLSX.read(fileData,{type: 'binary'});
+    			workbook.SheetNames.forEach(function(sheetName)
+    			{
+    				var rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName])
+    				excelJsonObj = rowObject;
+    			});
+
+    			for(var i =0;i< excelJsonObj.length;i++)
+    			{
+    				var data = excelJsonObj[i];
+    				$('#myTable tbody:last-child').append("<tr><td>"+data.ID+"</td><td>"+data.Predecessors+"</td><td>"+data.O+"</td></tr>");
+
+    			}
+    		};
+    		reader.readAsBinaryString(input.files[0]);
+    	};
+    }
+
+
+})();
